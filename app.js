c5502eb81d0b7b86f426aa884eedb65d30208fff
midoc05 <dominique.croce@gmail.com>
2021-09-28 23:17:49 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const server = express(); 
+const path = require("path"); 
+
+server.use ('/static', express.static('/public'))
+
+server.get ('/', (req,res) => {
+    res.send('/views/index.html')
+    })
+
+server.listen (3030, ()=>{
+console.log('Server En Marche')
+})
