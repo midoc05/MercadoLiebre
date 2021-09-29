2507c45733d0d6929f89e4535ff610692a704d84
Dante Muniz Torres <muniztorresdante@gmail.com>
2021-09-29 18:50:46 -0300

    agregado metodo sendFile y html home, para reemplazar index

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const server = express(); 
+
 const path = require("path"); 
 
-server.use ('/static', express.static('/public'))
 
-server.get ('/', (req,res) => {
-    res.send('/views/index.html')
-    })
+const publicPath = path.resolve(__dirname, './public');
+
+server.use(express.static(publicPath))
+
+
+
 
 server.listen (3030, ()=>{
 console.log('Server En Marche')
 })
+
+
+server.get ('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'))
+    })
+
