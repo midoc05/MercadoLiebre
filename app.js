a83790c6a95bc413eb9d460fd2f8c065088411e1
midoc05 <dominique.croce@gmail.com>
2021-10-14 23:03:32 -0300

    agrego login.html

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ server.get('/register.html', (req, res) => {
     res.sendFile(path.join(__dirname, './views/register.html'))
 })
 
+server.get('/login.html', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'))
+})
+
 server.listen(3030, () => {
     console.log('Server En Marche')
 })
